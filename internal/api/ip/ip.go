f52ea5ea82370484743e4da51d12dcd90abb3cef
Henry Rausch <44985923+HRA42@users.noreply.github.com>
2024-05-21 07:36:52 +0200

    Refactor IP handling in HandleCurrentIP function

--- internal/api/ip/ip.go
+++ internal/api/ip/ip.go
@@ -4,8 +4,20 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// return the current user IPv4 IP of the request
 func HandleCurrentIP(c *fiber.Ctx) error {
-	ip := c.IPs()[0]
+	// get the current user IPs
+	ips := c.IPs()
+	var ip string
+
+	if len(ips) > 0 {
+		ip = ips[0]
+	} else {
+		// fallback to direct IP if no forwarded IPs are found
+		ip = c.IP()
+	}
+
+	// json return of the IP
 	return c.JSON(fiber.Map{
 		"ip": ip,
 	})
