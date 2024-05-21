ed16e0d9f1c01ddfed46e755c5e703811580bb2f
Henry Rausch <44985923+HRA42@users.noreply.github.com>
2024-05-21 16:37:31 +0200

    Add logging and middleware, update API routes

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,23 +1,30 @@
 package api
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/hra42/Go-API/internal/api/ip"
+	"github.com/hra42/Go-API/internal/api/middleware"
 	"github.com/hra42/Go-API/internal/api/time"
 )
 
 func StartServer() {
+	log.Println("Starting server")
 	app := fiber.New(fiber.Config{
 		ServerHeader: "X-Forwarded-For",
 	})
 
+	middleware.Init()
+
 	app.Get("/", handleIndex)
 
 	// time
-	app.Get("/api/v1/time/current", time.HandleCurrent)
+	app.Get("/api/v1/current/time",middleware.AuthRequired, time.HandleCurrent)
 
 	// IPs
-	app.Get("/api/v1/ip/current", ip.HandleCurrentIP)
+	app.Get("/api/v1/current/ip/", middleware.AuthRequired, ip.HandleCurrentIP)
 
+	// start the server
 	app.Listen(":3000")
 }
